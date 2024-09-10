c48aef72df14a254310c6fb18c76af5a2ed524c7
BalancedProtector <139424334+BalancedProtector@users.noreply.github.com>
2024-09-10 00:50:00 -0400

    Pushing temp finish for the Contact Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from './header';
 import HomeContent from './home-content';
 import AboutMe from './about-me';
 import Portfolio from './portfolio';
-import ContactMe from './contact-me';
+import TempContact from './temp-contact';
+/*import ContactMe from './contact-me';*/
 import AllProjects from './all-projects';
 import ProjectDetail from './project-detail';
 import Footer from './footer';
@@ -16,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomeContent />} />
           <Route path="/about" element={<AboutMe />} />
-          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/contact" element={<TempContact />} />
           <Route path="/projects/portfolio" element={<Portfolio />} />
           <Route path="/projects/all" element={<AllProjects />} />
           <Route path="/projects/:projectId" element={<ProjectDetail />} />
---
+++ src/temp-contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact-me.css';
+
+const TempContact = () => {
+    return (
+        <div className="contact-me">
+            <h1>Let's talk, we can hammer out the details together</h1>
+            <h3>You can reach me preferrably by Email @ Stevemartin44224@gmail.com</h3>
+        </div>
+    )
+}
+export default TempContact;
